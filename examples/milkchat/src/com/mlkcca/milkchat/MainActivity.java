62f764cecc424e720ec5bbacc4e4458894e36238
syuhei176 <syuhei176@gmail.com>
2014-03-14 18:43:05 +0900

    update example
    update milk cocoa.jar

--- examples/milkchat/src/com/mlkcca/milkchat/MainActivity.java
+++ examples/milkchat/src/com/mlkcca/milkchat/MainActivity.java
@@ -1,23 +1,216 @@
 package com.mlkcca.milkchat;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+import javax.net.ssl.SSLContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.mlkcca.android.client.milkcocoa.Callback;
+import com.mlkcca.android.client.milkcocoa.DataStore;
+import com.mlkcca.android.client.milkcocoa.MilkCocoa;
+import com.mlkcca.android.client.milkcocoa.Query;
 
 public class MainActivity extends Activity {
 
+	private EditText editText;
+	private ArrayAdapter<String> adapter;
+	private SocketIO socket;
+    private Handler handler = new Handler();
+	private MilkCocoa milkcocoa;
+	private DataStore messagesDataStore;
+	
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+
+		// ListViewの設定
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		ListView listView = (ListView)findViewById(R.id.listView1);
+		listView.setAdapter(adapter);
+
+		editText = (EditText)findViewById(R.id.editText1);
+
+		try {
+			connect();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
     }
 
+    private void connect() {
+        this.milkcocoa = new MilkCocoa();
+		this.messagesDataStore = MainActivity.this.milkcocoa.dataStore("message");
+    	try {
+			//milkcocoa.init("http://10.0.2.2:3000", "mlkdev-000h", new Callback() {
+			milkcocoa.init("https://mlkcca.com", "chatapp", new Callback() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+				@Override
+				public void callback(Object err) {
+					Query query;
+					try {
+						query = MainActivity.this.messagesDataStore.query(new JSONObject());
+						query.limit(25);
+						query.desort("date");
+						query.done(new Callback() {
+
+							@Override
+							public void callback(Object arg0) {
+								final JSONArray messages = (JSONArray)arg0;
+								
+								new Thread(new Runnable() {
+									public void run() {
+									handler.post(new Runnable() {
+										public void run() {
+												for(int i=0;i < messages.length();i++) {
+													try {
+														adapter.insert(messages.getJSONObject(i).getString("content"), i);
+													} catch (JSONException e) {
+														// TODO Auto-generated catch block
+														e.printStackTrace();
+													}
+												}
+											}
+										});
+									}
+								}).start();
+							}});
+					} catch (JSONException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						MainActivity.this.messagesDataStore.on("push", new Callback() {
+
+							@Override
+							public void callback(Object arg0) {
+								final JSONObject pushed = (JSONObject)arg0;
+								new Thread(new Runnable() {
+									public void run() {
+									handler.post(new Runnable() {
+										public void run() {
+												String content = "";
+												try {
+													content = pushed.getJSONObject("value").getString("content");
+												} catch (JSONException e) {
+													// TODO Auto-generated catch block
+													e.printStackTrace();
+												}
+												adapter.insert(content, 0);
+											}
+										});
+									}
+								}).start();
+							}
+							
+						});
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			});
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
-    
+
+	private IOCallback iocallback = new IOCallback() {
+
+		@Override
+		public void onConnect() {
+		    System.out.println("onConnect");
+		}
+
+		@Override
+		public void onDisconnect() {
+		    System.out.println("onDisconnect");
+		}
+
+		@Override
+		public void onMessage(JSONObject json, IOAcknowledge ack) {
+			System.out.println("onMessage");
+		}
+
+		@Override
+		public void onMessage(String data, IOAcknowledge ack) {
+		    System.out.println("onMessage");
+		}
+
+		@Override
+		public void on(String event, IOAcknowledge ack, Object... args) {
+			final JSONObject message = (JSONObject)args[0];
+
+			new Thread(new Runnable() {
+				public void run() {
+				handler.post(new Runnable() {
+					public void run() {
+						try {
+							if(message.getString("message") != null) {
+								// メッセージが空でなければ追加
+								adapter.insert(message.getString("message"), 0);
+							}
+
+							} catch (JSONException e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+			}).start();
+		}
+
+		@Override
+		public void onError(SocketIOException socketIOException) {
+		    System.out.println("onError");
+		    socketIOException.printStackTrace();
+		}
+    };
+
+    public void sendEvent(View view){
+		// 文字が入力されていなければ何もしない
+		if (editText.getText().toString().length() == 0) {
+		    return;
+		}
+		
+		JSONObject params = new JSONObject();
+		try {
+			params.put("content", editText.getText().toString());
+			Date date = new Date();
+			params.put("date", date.toString());
+			this.messagesDataStore.push(params);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+    	// テキストフィールドをリセット
+    	editText.setText("");
+    }
 }
